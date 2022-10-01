1571efcc883f0e0c50fdcad4e24e9502f33ab994
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-10-01 15:34:38 -0300

    feat: `inicio` component

--- dynamox-landing-page-nextjs-mui/src/pages/index.tsx
+++ dynamox-landing-page-nextjs-mui/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Header from '../components/Header';
+import Inicio from '../components/Inicio';
 
 const Home: NextPage = () => {
   return (
@@ -10,6 +11,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/images/logo-dynamox.png" />
       </Head>
       <Header />
+      <Inicio />
     </div>
   );
 };
