8fadfffaadc814148e11187633e59bcdf33c3f62
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-10-01 15:51:03 -0300

    feat: `sensores` component

--- dynamox-landing-page-nextjs-mui/src/pages/index.tsx
+++ dynamox-landing-page-nextjs-mui/src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Header from '../components/Header';
 import Inicio from '../components/Inicio';
+import Sensores from '../components/Sensores';
 
 const Home: NextPage = () => {
   return (
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
       </Head>
       <Header />
       <Inicio />
+      <Sensores />
     </div>
   );
 };
