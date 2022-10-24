c03281badd6570c3e5c5180722279a29680a204f
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-10-24 15:33:11 -0300

    feat: private route

---
+++ desafio-dynamox-auth-crud/src/routes/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { GetData } from '../services/storage';
+
+const PrivateRoute = () => {
+  const accessToken = GetData('accessToken');
+
+  return accessToken ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
--- desafio-dynamox-auth-crud/src/routes/index.jsx
+++ desafio-dynamox-auth-crud/src/routes/index.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
+import ListProducts from '../pages/ListProducts';
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route element={<PrivateRoute />}>
+        <Route path="/products" element={<ListProducts />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   </BrowserRouter>
 );
